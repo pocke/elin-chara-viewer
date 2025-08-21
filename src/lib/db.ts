019bdc8b0785b0372363d0b16442f165bc188b28
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2025-08-21 10:10:29 +0900

    Memonize DB access

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,18 @@ import path from 'path';
 import { loadCsv } from './csvLoader';
 import { z } from 'zod';
 
+const cache = new Map<string, unknown[]>();
+
 export const all = async <T>(tableName: string, schema: z.ZodType<T>) => {
+  if (cache.has(tableName)) {
+    return cache.get(tableName) as T[];
+  }
+
   const csvPath = path.join(
     process.cwd(),
     `db/EA 23.173 Patch 1/${tableName}.csv`
   );
-  return loadCsv(csvPath, schema);
+  const result = await loadCsv(csvPath, schema);
+  cache.set(tableName, result);
+  return result;
 };
