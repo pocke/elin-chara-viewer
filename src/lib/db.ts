e1d924023cf88c2a7e634ca10d8ea9840afda8b4
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2026-03-18 14:24:06 +0900

    EA 23.286 Patch 1 Nightly

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,11 +9,11 @@ import eaJobsContent from '../../db/EA 23.282 Patch 3/jobs.csv';
 import eaTacticsContent from '../../db/EA 23.282 Patch 3/tactics.csv';
 
 // Import CSV files for Nightly version (currently using EA data as placeholder)
-import nightlyCharasContent from '../../db/EA 23.285/charas.csv';
-import nightlyElementsContent from '../../db/EA 23.285/elements.csv';
-import nightlyRacesContent from '../../db/EA 23.285/races.csv';
-import nightlyJobsContent from '../../db/EA 23.285/jobs.csv';
-import nightlyTacticsContent from '../../db/EA 23.285/tactics.csv';
+import nightlyCharasContent from '../../db/EA 23.286 Patch 1/charas.csv';
+import nightlyElementsContent from '../../db/EA 23.286 Patch 1/elements.csv';
+import nightlyRacesContent from '../../db/EA 23.286 Patch 1/races.csv';
+import nightlyJobsContent from '../../db/EA 23.286 Patch 1/jobs.csv';
+import nightlyTacticsContent from '../../db/EA 23.286 Patch 1/tactics.csv';
 
 export type GameVersion = 'EA' | 'Nightly';
 
