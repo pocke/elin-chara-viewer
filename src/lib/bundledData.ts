16885607f7d8a1cf4bf2cccd08e3407b54e214af
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2026-08-17 23:33:30 +0900

    EA 23.336 Nightly

--- src/lib/bundledData.ts
+++ src/lib/bundledData.ts
@@ -6,11 +6,11 @@ import eaRacesContent from '../../db/EA 23.325 Patch 2/races.csv';
 import eaJobsContent from '../../db/EA 23.325 Patch 2/jobs.csv';
 import eaTacticsContent from '../../db/EA 23.325 Patch 2/tactics.csv';
 
-import nightlyCharasContent from '../../db/EA 23.335 Patch 2/charas.csv';
-import nightlyElementsContent from '../../db/EA 23.335 Patch 2/elements.csv';
-import nightlyRacesContent from '../../db/EA 23.335 Patch 2/races.csv';
-import nightlyJobsContent from '../../db/EA 23.335 Patch 2/jobs.csv';
-import nightlyTacticsContent from '../../db/EA 23.335 Patch 2/tactics.csv';
+import nightlyCharasContent from '../../db/EA 23.336/charas.csv';
+import nightlyElementsContent from '../../db/EA 23.336/elements.csv';
+import nightlyRacesContent from '../../db/EA 23.336/races.csv';
+import nightlyJobsContent from '../../db/EA 23.336/jobs.csv';
+import nightlyTacticsContent from '../../db/EA 23.336/tactics.csv';
 
 import featModifierEaJson from '../generated/featModifier.ea.json';
 import featModifierNightlyJson from '../generated/featModifier.nightly.json';
