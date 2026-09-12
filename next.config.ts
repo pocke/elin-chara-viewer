aec7ada4bf945be55c9e2bb7cf4ac49015849887
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2026-09-12 21:22:46 +0900

    EA 23.344 Patch 1 Nightly

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
   env: {
     GIT_LAST_COMMIT_DATE: gitInfo.lastCommitDate,
     ELIN_EA_VERSION: 'EA 23.338 Patch 2',
-    ELIN_NIGHTLY_VERSION: 'EA 23.344',
+    ELIN_NIGHTLY_VERSION: 'EA 23.344 Patch 1',
   },
   turbopack: {
     rules: {
